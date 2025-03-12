eb7e15406e70c1e260a89c7796b5d1c206718274
Girts Silis <girts.silis@gmail.com>
2025-03-12 17:55:25 -0500

    + dice component

---
+++ app/components/dice/dice.tsx
@@ -0,0 +1,21 @@
+import './dice.css';
+
+export type DiceProps = {
+  display: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+};
+
+export default function Dice({
+  display,
+}: DiceProps) {
+  const classNames = ['dice', `roll-${display}`];
+
+  return <div className={ classNames.join(' ') }>
+    <div className="dot column-1 row-1"></div>
+    <div className="dot column-1 row-2"></div>
+    <div className="dot column-1 row-3"></div>
+    <div className="dot column-3 row-1"></div>
+    <div className="dot column-3 row-2"></div>
+    <div className="dot column-3 row-3"></div>
+    <div className="dot column-2 row-2"></div>
+  </div>;
+}
